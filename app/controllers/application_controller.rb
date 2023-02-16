4092f11af56ad1ff4654a17480230f1aea81c4e4
William Baele <baelewilliam1999@gmail.com>
2023-02-16 10:29:27 +0100

    Added sign up/sign in methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    flash[:success] = "Succesfully signed in"
+    root_path
+  end
+  def after_sign_up_path_for(_resource)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
   # Defines the root path route ("/")
