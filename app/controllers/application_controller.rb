5d428df0f0d7e1a2c5ca6fa534f6e01177e4fce0
William Baele <baelewilliam1999@gmail.com>
2023-02-16 11:43:31 +0100

    Fixed signed up error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name])
+  end
+
   def after_sign_in_path_for(_resource)
-    flash[:success] = "Succesfully signed in"
+    flash[:success] = "Succesfully logged in"
     root_path
   end
   def after_sign_up_path_for(_resource)
+    flash[:success] = "Succesfully signed in"
     root_path
   end
 end
