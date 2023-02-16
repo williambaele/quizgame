a3cd37d066747a24fbc7094aa308ce4ed621261e
William Baele <baelewilliam1999@gmail.com>
2023-02-16 16:33:47 +0100

    Added play page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
+  get '/play', to: 'pages#play'
   # Defines the root path route ("/")
   # root "articles#index"
 end
